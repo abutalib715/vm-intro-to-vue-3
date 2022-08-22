b6dbd4cf974180013b40e4a304d0265b5f881295
Talib <abutalib715@gmail.com>
2022-08-23 01:23:50 +0600

    learned looping over data via for loop

--- main.js
+++ main.js
@@ -7,7 +7,16 @@ const app = Vue.createApp({
             url: 'http://google.com',
             inStock: false,
             inventory: 8,
-            onSale: true
+            onSale: true,
+            details: [
+                '50% cotton','20% wool', '30% silk'
+            ],
+            variants: [
+                {id: 2233, color:'green'},
+                {id: 2234, color:'red'}
+            ],
+            sizes: [30,32,34,36,41,40]
+
         }
     }
 })
