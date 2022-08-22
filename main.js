b1bb791c678a2aa0f4a18709b0166e6206bb5786
Talib <abutalib715@gmail.com>
2022-08-23 00:26:31 +0600

    vue app just mounted

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            cart: [],
-            premium: true  
-        }
-    },
-    methods:{
-        updateCart(id){
-            this.cart.push(id)
-        },
-        decCart(id){
-            this.cart.pop(id)
+    data() {
+        return {
+            product: 'Socks'
         }
     }
 })
- 
