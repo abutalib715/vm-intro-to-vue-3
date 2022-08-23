1ce4a2aef9b9bf7e372a89748f8d26a3173a8973
Talib <abutalib715@gmail.com>
2022-08-23 09:32:35 +0600

    learned event handling and methods

--- main.js
+++ main.js
@@ -12,11 +12,24 @@ const app = Vue.createApp({
                 '50% cotton','20% wool', '30% silk'
             ],
             variants: [
-                {id: 2233, color:'green'},
-                {id: 2234, color:'red'}
+                {id: 2233, color:'green', image: './assets/images/socks_green.jpg'},
+                {id: 2234, color:'blue', image: './assets/images/socks_blue.jpg'}
             ],
-            sizes: [30,32,34,36,41,40]
+            sizes: [30,32,34,36,41,40],
+            cart: 0
 
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart(){
+            if(this.cart > 0)
+                this.cart--
+        },
+        updateImage(image){
+            this.image = image
         }
     }
 })
